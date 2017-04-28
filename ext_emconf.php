cf6d48b30d32eeaad2159a01548ed8d95bd52c02
Dragan Tomic <dragan.tomic@aoe.com>
2017-04-28 11:50:56 +0200

    extension kickstart

---
+++ ext_emconf.php
@@ -0,0 +1,38 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array (
+	'title' => 'GeoIP detection',
+	'description' => 'Detect client location',
+	'category' => '',
+	'shy' => 0,
+	'version' => '0.1.0',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'beta',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Dragan Tomic, Marko Milojevic',
+	'author_email' => 'dragan.tomic@aoe.com, marko.milojevic@aoe.com',
+	'author_company' => 'AOE GmbH',
+	'CGLcompliance' => NULL,
+	'CGLcompliance_note' => NULL,
+	'constraints' => 
+	array (
+		'depends' => array(
+			'php' => '5.5.0-0.0.0',
+			'typo3' => '7.0.0-7.9.9',
+		),
+		'conflicts' => 
+		array (
+		),
+		'suggests' => 
+		array (
+		),
+	),
+);
